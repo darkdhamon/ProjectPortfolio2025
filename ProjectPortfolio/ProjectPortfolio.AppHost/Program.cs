f6c9e187521ea082ad4ae43033f3a67cdca6dbcf
Bronze Brown <dhamon.nightstar@gmail.com>
2025-07-23 11:42:47 -0400

    Add Web Apit

--- ProjectPortfolio/ProjectPortfolio.AppHost/Program.cs
+++ ProjectPortfolio/ProjectPortfolio.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.WebAppRazor>("webapprazor");
 
+builder.AddProject<Projects.WebAPI>("webapi");
+
 builder.Build().Run();
