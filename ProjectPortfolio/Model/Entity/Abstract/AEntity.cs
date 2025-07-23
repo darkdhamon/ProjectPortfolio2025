2b3087b9609bba55c8a448f7293099e48d9bf8be
Bronze Brown <dhamon.nightstar@gmail.com>
2025-07-23 12:49:49 -0400

    Abstract

--- ProjectPortfolio/Model/Entity/Abstract/AEntity.cs
+++ ProjectPortfolio/Model/Entity/Abstract/AEntity.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Model.Entity.Abstract
 {
-    internal class AEntity
+    public abstract class AEntity
     {
+        [Key]
+        public int ID { get; set; }
     }
 }
